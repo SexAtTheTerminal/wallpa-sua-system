9ff0b8eabc974d619527d5e4d1d931cc13167628
Isaac <gabrielcuba2017@gmail.com>
2026-06-09 03:40:47 -0500

    relogin

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SUPABASE_URL: 'https://szueqanbrrleewlyyifc.supabase.co',
-  SUPABASE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6dWVxYW5icnJsZWV3bHl5aWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NTU3NTQsImV4cCI6MjA2MzAzMTc1NH0.p17XC0So6tntLHXB4suahgsi0uuYALauW0DMSJbGsww',
+  SUPABASE_URL: 'https://crzylwkfqabckdqrawaq.supabase.co',
+  SUPABASE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyenlsd2tmcWFiY2tkcXJhd2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NzU5OTYsImV4cCI6MjA5NjU1MTk5Nn0.wQc523CduDNpiPHhlvHhoVd5z0rmNnxMtuIbWzOW-mM',
 };
